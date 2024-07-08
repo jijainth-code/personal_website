09d8818c1f0cd2e4a04cabaef4895283998ed27c
jijainth-code <jijainth.dhinakaran@stud.th-deg.de>
2024-07-08 23:13:13 +0200

    updated node package , played around with presonal portfolio template

---
+++ src/component/banner/jaiBannerThree.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+import Tilty from 'react-tilty';
+
+
+const BannerThree = () => {
+    return (
+            <div className="banner banner-style-3">
+            <div className="container">
+                <div className="row align-items-center">
+                <div className="col-lg-6">
+                    <div className="banner-content">
+                        <AnimationOnScroll  animateIn="slideInUp" duration={1} animateOnce={true} delay={100}>
+                            <span className="subtitle">JIJAINTH DHINAKARAN</span>
+                        </AnimationOnScroll>
+                        <AnimationOnScroll  animateIn="slideInUp" duration={1} animateOnce={true} delay={200}>
+                            <h1 className="title">Professional Full Stack and AI Engineer</h1>
+
+                            
+                        </AnimationOnScroll>
+                        <AnimationOnScroll  animateIn="slideInUp" duration={1} animateOnce={true} delay={200}>
+                        <div className="btn-group">
+                            <a href="https://www.linkedin.com/in/jijainth/recent-activity/all/" className="axil-btn btn-fill-primary btn-large">Read my Articles</a>
+                        </div>
+                        </AnimationOnScroll>
+                    </div>
+                </div>
+                <div className="col-lg-6">
+                    <div className="banner-thumbnail">
+                    <div className="large-thumb">
+                        <AnimationOnScroll  animateIn="slideInUp" duration={1} animateOnce={true} delay={200}>
+                            <Tilty perspective={2000} reset={false}>
+                                <img src={process.env.PUBLIC_URL + "/images/banner/me.png"} alt="Shape" />
+                            </Tilty>
+                        </AnimationOnScroll>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <ul className="list-unstyled shape-group-20">
+                <li className="shape shape-1">
+                <img src={process.env.PUBLIC_URL + "/images/others/bubble-32.png"} alt="Bubble" />
+                </li>
+                <li className="shape shape-2">
+                <img src={process.env.PUBLIC_URL + "/images/others/bubble-33.png"} alt="Bubble" />
+                </li>
+                <li className="shape shape-4">
+                <img src={process.env.PUBLIC_URL + "/images/others/bubble-14.png"} alt="Bubble" />
+                </li>
+                <li className="shape shape-5">
+                <img src={process.env.PUBLIC_URL + "/images/others/bubble-4.png"} alt="Bubble" />
+                </li>
+                <li className="shape shape-6">
+                <img src={process.env.PUBLIC_URL + "/images/others/bubble-30.png"} alt="Bubble" />
+                </li>
+                <li className="shape shape-7">
+                <img src={process.env.PUBLIC_URL + "/images/others/bubble-31.png"} alt="Bubble" />
+                </li>
+                <li className="shape shape-8 marque-images" />
+            </ul>
+        </div>
+
+    )
+}
+
+export default BannerThree;
---
+++ src/pages/PersonalPortfolio.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import HeaderThree from '../common/header/jaiHeaderThree';
+import SEO from '../common/SEO';
+import BannerThree from '../component/banner/jaiBannerThree';
+import ProjectTwo from '../component/project/ProjectTwo';
+import SectionTitle from '../elements/section-title/SectionTitle';
+import ColorSwitcher from '../elements/switcher/ColorSwitcher';
+import BrandOne from '../component/brand/BrandOne';
+import ProjectThree from '../component/project/ProjectThree';
+import TestimonialTwo from '../component/testimonial/TestimonialTwo';
+import FooterTwo from '../common/footer/FooterTwo';
+import FormThree from '../component/contact/FormThree';
+
+const PersonalPortfolio = () => {
+
+    return (
+        <>
+        <SEO title="Personal Portfolio"/>
+        <ColorSwitcher />
+        <main className="main-wrapper">
+            <HeaderThree />
+            <BannerThree />
+            <ProjectTwo />
+            <ProjectThree />
+
+            <BrandOne />
+            <TestimonialTwo />
+
+            <div className="section-padding bg-color-dark">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6">
+                            <div className="contact-address mb--30">
+                            <SectionTitle 
+                                subtitle="Contact me ?"
+                                title="Let’s work together"
+                                description=""
+                                textAlignment="heading-light-left"
+                                textColor=""
+                            />
+                            <div className="address-list">
+                                <div className="address">
+                                <h6 className="title">E-Mail</h6>
+                                <p>jijainthdhinakaran@aol.com</p>
+                                </div>
+                                <div className="address">
+                                <h6 className="title">Address</h6>
+                                <p>München, Bavaria, Germany </p>
+                                </div>
+                                <div className="address">
+                                <h6 className="title">Phone</h6>
+                                <p>+491749566895</p>
+                                from 11:00 - 19:30
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="contact-form-box mb--30">
+                                <h3 className="title">send a quick message to me here !</h3>
+                                <FormThree />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <FooterTwo />
+
+        </main>
+        </>
+    )
+}
+
+export default PersonalPortfolio;
+
